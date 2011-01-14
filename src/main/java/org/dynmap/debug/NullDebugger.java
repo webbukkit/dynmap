686b09bc9da4355d55a45e03a99b43218e956a69
FrozenCow <frozencow@gmail.com>
2011-01-14 23:41:13 +0100

    Tiles will now also generate when destroying blocks.

--- src/main/java/org/dynmap/debug/NullDebugger.java
+++ src/main/java/org/dynmap/debug/NullDebugger.java
@@ -1,15 +1,12 @@
 package org.dynmap.debug;
 
 public class NullDebugger implements Debugger {
-	@Override
 	public void debug(String message) {
 	}
 
-	@Override
 	public void error(String message) {
 	}
 
-	@Override
 	public void error(String message, Throwable thrown) {
 	}
 
