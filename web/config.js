75efba1425b87e1cf25f9b735b12d35c646cc0ab
FrozenCow <frozencow@gmail.com>
2011-01-20 23:29:38 +0100

    Fixed comment in config.js.

--- web/config.js
+++ web/config.js
@@ -1,7 +1,7 @@
 var config = {
         tileUrl:     'tiles/',
         updateUrl:   'up/',                             // For Apache and lighttpd
-//      updateUrl:   'up/default.aspx?lasttimestamp=',  // For IIS
+//      updateUrl:   'up.aspx?path=',                   // For IIS
         updateRate:  2000,                              // Seconds the map should poll for updates. (Seconds) * 1000. The default is 2000 (every 2 seconds).
         showPortraitsOnMap: true,
         showPortraitsInPlayerList: true,
