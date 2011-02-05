4f138a56da29eb96b33568086954ded6afa9e754
FrozenCow <frozencow@gmail.com>
2011-02-05 02:11:49 +0100

    Removed comments.

--- src/main/java/org/dynmap/DynmapPlugin.java
+++ src/main/java/org/dynmap/DynmapPlugin.java
@@ -80,22 +80,5 @@ public class DynmapPlugin extends JavaPlugin {
 		
 		getServer().getPluginManager().registerEvent(Event.Type.PLAYER_COMMAND, new DynmapPlayerListener(mapManager, playerList), Priority.Normal, this);
 		getServer().getPluginManager().registerEvent(Event.Type.PLAYER_CHAT, new DynmapPlayerListener(mapManager, playerList), Priority.Normal, this);
-		//getServer().getPluginManager().registerEvent(Event.Type.BLOCK_DESTROYED, listener, Priority.Normal, this);
-	/*	etc.getLoader().addListener(PluginLoader.Hook.COMMAND, listener, this, PluginListener.Priority.MEDIUM);
-		etc.getLoader().addListener(PluginLoader.Hook.BLOCK_CREATED, listener, this, PluginListener.Priority.MEDIUM);
-		etc.getLoader().addListener(PluginLoader.Hook.BLOCK_DESTROYED, listener, this, PluginListener.Priority.MEDIUM);
-		etc.getLoader().addListener(PluginLoader.Hook.LOGIN, listener, this, PluginListener.Priority.MEDIUM);
-
-		etc.getInstance().addCommand("/map_wait", " [wait] - set wait between tile renders (ms)");
-		etc.getInstance().addCommand("/map_stat", " - query number of tiles in render queue");
-		etc.getInstance().addCommand("/map_regen", " - regenerate entire map");
-		etc.getInstance().addCommand("/map_debug", " - send map debugging messages");
-		etc.getInstance().addCommand("/map_nodebug", " - disable map debugging messages");
-		etc.getInstance().addCommand("/addsign", " [name] - adds a named sign to the map");
-		etc.getInstance().addCommand("/removesign", " [name] - removes a named sign to the map");
-		etc.getInstance().addCommand("/listsigns", " - list all named signs");
-		etc.getInstance().addCommand("/tpsign", " [name] - teleport to a named sign");
-		*/
-		
 	}
 }
