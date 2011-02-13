42391cc4d57c0c8f7c338f200c99f0ba248f464b
FrozenCow <frozencow@gmail.com>
2011-02-13 23:30:19 +0100

    Readded getMinecraftTime.

--- web/minecraft.js
+++ web/minecraft.js
@@ -77,3 +77,20 @@ function getMinecraftHead(player,size,completed) {
 		completed(resizeImage(head,size));
 	}
 }
+
+function getMinecraftTime(servertime) {
+	servertime = parseInt(servertime);
+	var day = servertime >= 0 && servertime < 13700;
+	return {
+		servertime: servertime,
+		days: parseInt((servertime+8000) / 24000),
+		
+		// Assuming it is day at 8:00
+		hours: (parseInt(servertime / 1000)+8) % 24,
+		minutes: parseInt(((servertime / 1000) % 1) * 60),
+		seconds: parseInt(((((servertime / 1000) % 1) * 60) % 1) * 60),
+		
+		day: day,
+		night: !day
+	};
+}
