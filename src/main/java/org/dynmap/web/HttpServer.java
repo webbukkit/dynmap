e613fb14d727fb4d71d36f052a3b5b49b357dec0
Mike Primm <mike@primmhome.com>
2011-07-22 16:56:08 -0500

    Add support for enforcing banned-ips.txt on internal web server

--- src/main/java/org/dynmap/web/HttpServer.java
+++ src/main/java/org/dynmap/web/HttpServer.java
@@ -1,10 +1,16 @@
 package org.dynmap.web;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.InetAddress;
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketAddress;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.SortedMap;
 import java.util.TreeMap;
 import java.util.logging.Logger;
@@ -13,19 +19,20 @@ import org.dynmap.Log;
 
 public class HttpServer extends Thread {
     protected static final Logger log = Logger.getLogger("Minecraft");
-    protected static final String LOG_PREFIX = "[dynmap] ";
 
     private ServerSocket sock = null;
     private Thread listeningThread;
 
     private InetAddress bindAddress;
     private int port;
+    private boolean check_banned_ips;
 
     public SortedMap<String, HttpHandler> handlers = new TreeMap<String, HttpHandler>(Collections.reverseOrder());
 
-    public HttpServer(InetAddress bindAddress, int port) {
+    public HttpServer(InetAddress bindAddress, int port, boolean check_banned_ips) {
         this.bindAddress = bindAddress;
         this.port = port;
+        this.check_banned_ips = check_banned_ips;
     }
 
     public InetAddress getAddress() {
@@ -49,6 +56,11 @@ public class HttpServer extends Thread {
             while (listeningThread == Thread.currentThread()) {
                 try {
                     Socket socket = s.accept();
+                    if(checkForBannedIp(socket.getRemoteSocketAddress())) {
+                    	try { socket.close(); } catch (IOException iox) {}
+                    	socket = null;
+                    }
+
                     HttpServerConnection requestThread = new HttpServerConnection(socket, this);
                     requestThread.start();
                 } catch (IOException e) {
@@ -74,5 +86,65 @@ public class HttpServer extends Thread {
         } catch (IOException e) {
             Log.warning("Exception while closing socket for webserver shutdown", e);
         }
+    }
+    
+    private HashSet<String> banned_ips = new HashSet<String>();
+    private HashSet<String> banned_ips_notified = new HashSet<String>();
+    private long last_loaded = 0;
+    private long lastmod = 0;
+    private static final long BANNED_RELOAD_INTERVAL = 15000;	/* Every 15 seconds */
+    
+    private void loadBannedIPs() {
+    	banned_ips.clear();
+    	banned_ips_notified.clear();
+    	File f = new File("banned-ips.txt");
+    	if(f.exists() == false)
+    		return;
+    	if(f.lastModified() == lastmod) {
+    		return;
+    	}
+    	lastmod = f.lastModified();
+    	BufferedReader rdr = null;
+    	try {
+    		rdr = new BufferedReader(new FileReader(f));
+    		String line;
+    		while((line = rdr.readLine()) != null) {
+    			line = line.trim().toLowerCase();	/* Trim it and case normalize it */
+    			if((line.length() == 0) || (line.charAt(0) == '#')) {	/* Blank or comment? */
+    				continue;
+    			}
+    			banned_ips.add(line);
+    		}
+    	} catch (IOException iox) {
+    		Log.severe("Error reading banned-ips.txt!");
+    	} finally {
+    		if(rdr != null) {
+    			try { rdr.close(); } catch (IOException iox) {}
+    			rdr = null;
+    		}
+    	}
+    }
+    /* Return true if address is banned */
+    public boolean checkForBannedIp(SocketAddress socketAddress) {
+    	if(!check_banned_ips)
+    		return false;
+    	
+    	long t = System.currentTimeMillis();
+    	if((t < last_loaded) || ((t-last_loaded) > BANNED_RELOAD_INTERVAL)) {
+    		loadBannedIPs();
+    		last_loaded = t;
+    	}
+    	/* Follow same technique as MC uses - toString the SocketAddress and clip out string between "/" and ":" */
+    	String ip = socketAddress.toString();
+    	ip = ip.substring(ip.indexOf("/") + 1);
+    	ip = ip.substring(0, ip.indexOf(":"));
+    	if(banned_ips.contains(ip)) {
+    		if(banned_ips_notified.contains(ip) == false) {
+    			Log.info("Rejected connection by banned IP address - " + socketAddress.toString());
+    			banned_ips_notified.add(ip);
+    		}
+    		return true;
+    	}
+    	return false;
     }
 }
