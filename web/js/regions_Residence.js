d483eb1e6faa9d8e22a2630baf03c2f34ee0d6bb
Mike Primm <mike@primmhome.com>
2011-07-27 22:42:55 -0500

    Fix Residence support in updated UI

--- web/js/regions_Residence.js
+++ web/js/regions_Residence.js
@@ -4,16 +4,16 @@ regionConstructors['Residence'] = function(dynmap, configuration) {
 		return boxCreator(area.X1, area.X2, area.Y1, area.Y2, area.Z1, area.Z2);
 	}
 
-	$.getJSON('standalone/res_' + worldName + '.json', function(data) {
+	$.getJSON('standalone/res_' + configuration.worldName + '.json', function(data) {
 		var boxLayers = [];
 		$.each(data, function(name, residence) {
-			if(worldName == residence.Permissions.World) {
+			if(configuration.worldName == residence.Permissions.World) {
 				$.each(residence.Areas, function(name, area) {
 					var boxLayer = configuration.createBoxLayer(area.X1, area.X2, area.Y1, area.Y2, area.Z1, area.Z2);
 					
 					boxLayer.bindPopup(configuration.createPopupContent(name, $.extend(residence, {
 						owners: { players: [residence.Permissions.Owner] },
-						flags: region.Permissions.AreaFlags
+						flags: residence.Permissions.AreaFlags
 					})));
 					
 					boxLayers.push(boxLayer);
