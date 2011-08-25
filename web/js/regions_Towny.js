627cef720c4e57409cf95a044f4528d8b7ebf0a7
Mike Primm <mike@primmhome.com>
2011-08-24 22:11:31 -0500

    Towny fixes - multiple areas on multiple worlds per town

--- web/js/regions_Towny.js
+++ web/js/regions_Towny.js
@@ -1,14 +1,12 @@
 regionConstructors['Towny'] = function(dynmap, configuration) {
 	// Helper function.
-	function createOutlineFromRegion(name, region, outCreator) {
+	function createOutlineFromRegion(name, region, points, outCreator) {
 		var xarray = [];
 		var zarray = [];
-		if(region.points) {
-			var i;
-			for(i = 0; i < region.points.length; i++) {
-				xarray[i] = region.points[i].x;
-				zarray[i] = region.points[i].z;
-			}
+		var i;
+		for(i = 0; i < points.length; i++) {
+			xarray[i] = points[i].x;
+			zarray[i] = points[i].z;
 		}
 		var ymin = 64;
 		var ymax = 65;
@@ -20,14 +18,17 @@ regionConstructors['Towny'] = function(dynmap, configuration) {
 	$.getJSON('standalone/'+regionFile, function(data) {
 		var boxLayers = [];
 		$.each(data, function(name, region) {
-			var outLayer = createOutlineFromRegion(name, region, configuration.createOutlineLayer);
-			if (outLayer) {
-			    outLayer.bindPopup(configuration.createPopupContent(name, 
-			    	$.extend(region, {
-						owners: { players: [region.mayor] },
-						members: { players: [ region.residents ] }
-					})));
-			    boxLayers.push(outLayer);
+			var i;
+			for(i = 0; i < region.points.length; i++) {
+				var outLayer = createOutlineFromRegion(name, region, region.points[i], configuration.createOutlineLayer);
+				if (outLayer) {
+				    outLayer.bindPopup(configuration.createPopupContent(name, 
+				    	$.extend(region, {
+							owners: { players: [region.mayor] },
+							members: { players: [ region.residents ] }
+						})));
+			    	boxLayers.push(outLayer);
+				}
 			}
 		});
 		configuration.result(new L.LayerGroup(boxLayers));
