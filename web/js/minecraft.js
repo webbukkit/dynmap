83642a62915d94e27656b1fed6e9680cf26c9d84
Mike Primm <mike@primmhome.com>
2011-09-04 02:03:52 -0500

    Put it back - not sure other fix did anything

--- web/js/minecraft.js
+++ web/js/minecraft.js
@@ -9,43 +9,11 @@ function createMinecraftHead(player,size,completed,failed) {
 	faceImage.src = dynmap.options.tileUrl + 'faces/' + size + 'x' + size + '/' + player + '.png';
 }
 
-var playerHeads = {};
-
 function getMinecraftHead(player,size,completed) {
-    var key = player + '.' + size;
-	var head = playerHeads[key];
-	// Synchronous
-	if (!completed) {
-		return (!head || head.working) ? null : head;
-	}
-
-	// Asynchronous
-	if (!head) {
-		playerHeads[key] = { working: true, hooks: [{f:completed}] };
-		createMinecraftHead(player, size, function(head) {
-			hooks = playerHeads[key].hooks;
-			playerHeads[key] = head;
-			var i;
-			for(i=0;i<hooks.length;i++) {
-				hooks[i].f(head);
-			}
-			head.working = false;
-		}, function() {
-
-		});
-	} else if (head.working) {
-		//console.log('Other process working on head of ',player,', will add myself to hooks...');
-		head.hooks[head.hooks.length] = {f:completed};
-	} else {
-		completed(head);
-	}
+	createMinecraftHead(player, size, completed, function() {
+		console.error('Failed to retrieve face of "', player, '" with size "', size, '"!')
+	});
 }
-
-//function getMinecraftHead(player,size,completed) {
-//	createMinecraftHead(player, size, completed, function() {
-//		console.error('Failed to retrieve face of "', player, '" with size "', size, '"!')
-//	});
-//}
 
 function getMinecraftTime(servertime) {
 	servertime = parseInt(servertime);
