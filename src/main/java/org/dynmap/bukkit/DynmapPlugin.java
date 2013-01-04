bd12420edd7d59e5dd36ac3fe276a3f2c13276a9
Mike Primm <mike@primmhome.com>
2013-01-03 21:58:58 -0600

    Add option to hide players on map that have invisibility potion effects

--- src/main/java/org/dynmap/bukkit/DynmapPlugin.java
+++ src/main/java/org/dynmap/bukkit/DynmapPlugin.java
@@ -63,6 +63,7 @@ import org.bukkit.plugin.Plugin;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffectType;
 import org.dynmap.DynmapAPI;
 import org.dynmap.DynmapChunk;
 import org.dynmap.DynmapCore;
@@ -607,6 +608,13 @@ public class DynmapPlugin extends JavaPlugin implements DynmapAPI {
         @Override
         public long getFirstLoginTime() {
             return offplayer.getFirstPlayed();
+        }
+        @Override
+        public boolean isInvisible() {
+            if(player != null) {
+                return player.hasPotionEffect(PotionEffectType.INVISIBILITY);
+            }
+            return false;
         }
     }
     /* Handler for generic console command sender */
