b61b5ca39ade53a3bba4b31999cdafce55037f38
Mike Primm <mike@primmhome.com>
2020-04-26 19:43:35 -0500

    More fixes

--- forge-1.13.2/src/main/java/org/dynmap/forge_1_13_2/ForgeMapChunkCache.java
+++ forge-1.13.2/src/main/java/org/dynmap/forge_1_13_2/ForgeMapChunkCache.java
@@ -961,7 +961,7 @@ public class ForgeMapChunkCache extends MapChunkCache
         	if (cp instanceof ChunkProviderServer)
         	{
                 cps = (ChunkProviderServer)cp;
-                Log.info("World " + dw.getName() + " has chunk provider " + cps.class.getName());
+                Log.info("World " + dw.getName() + " has chunk provider " + cps.getClass().getName());
         	}
         	else
         	{
