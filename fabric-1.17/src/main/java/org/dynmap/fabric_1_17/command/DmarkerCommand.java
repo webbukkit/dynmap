d5f026b533a4ab40ef57b482e68d30d77b33a52b
Kosma Moczek <kosma@kosma.pl>
2021-06-27 17:49:18 +0200

    fabric-1.17: add port

---
+++ fabric-1.17/src/main/java/org/dynmap/fabric_1_17/command/DmarkerCommand.java
@@ -0,0 +1,9 @@
+package org.dynmap.fabric_1_17.command;
+
+import org.dynmap.fabric_1_17.DynmapPlugin;
+
+public class DmarkerCommand extends DynmapCommandExecutor {
+    public DmarkerCommand(DynmapPlugin p) {
+        super("dmarker", p);
+    }
+}
---
+++ fabric-1.17/src/main/java/org/dynmap/fabric_1_17/command/DynmapCommand.java
@@ -0,0 +1,9 @@
+package org.dynmap.fabric_1_17.command;
+
+import org.dynmap.fabric_1_17.DynmapPlugin;
+
+public class DynmapCommand extends DynmapCommandExecutor {
+    public DynmapCommand(DynmapPlugin p) {
+        super("dynmap", p);
+    }
+}
