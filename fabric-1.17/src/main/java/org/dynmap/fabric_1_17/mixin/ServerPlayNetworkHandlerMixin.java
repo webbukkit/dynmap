0e54d23d6187fe45b4f193a6ec955b996be991c7
Kosma Moczek <kosma@kosma.pl>
2021-06-27 19:35:32 +0200

    fabric-1.17: restore web chat functionality

--- fabric-1.17/src/main/java/org/dynmap/fabric_1_17/mixin/ServerPlayNetworkHandlerMixin.java
+++ fabric-1.17/src/main/java/org/dynmap/fabric_1_17/mixin/ServerPlayNetworkHandlerMixin.java
@@ -1,5 +1,6 @@
 package org.dynmap.fabric_1_17.mixin;
 
+import net.minecraft.server.filter.TextStream;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.dynmap.fabric_1_17.event.ServerChatEvents;
@@ -15,14 +16,14 @@ public abstract class ServerPlayNetworkHandlerMixin {
     public ServerPlayerEntity player;
 
     @Inject(
-            method = "method_31286",
+            method = "handleMessage",
             at = @At(
                     value = "INVOKE",
-                    target = "Lnet/minecraft/server/PlayerManager;broadcastChatMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/MessageType;Ljava/util/UUID;)V",
+                    target = "Lnet/minecraft/server/PlayerManager;broadcast(Lnet/minecraft/text/Text;Ljava/util/function/Function;Lnet/minecraft/network/MessageType;Ljava/util/UUID;)V",
                     shift = At.Shift.BEFORE
             )
     )
-    public void onGameMessage(String string, CallbackInfo info) {
-        ServerChatEvents.EVENT.invoker().onChatMessage(player, string);
+    public void onGameMessage(TextStream.Message message, CallbackInfo info) {
+        ServerChatEvents.EVENT.invoker().onChatMessage(player, message.getRaw());
     }
 }
