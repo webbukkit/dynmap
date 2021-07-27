39a6cecb2be6b6604980614a481d2c19c9cb5864
Mike Primm <mike@primmhome.com>
2021-07-26 21:47:12 -0500

    Update build for 1.17.1 to 37.0.12 - get build functional

--- forge-1.17.1/src/main/java/org/dynmap/forge_1_17_1/ChunkSnapshot.java
+++ forge-1.17.1/src/main/java/org/dynmap/forge_1_17_1/ChunkSnapshot.java
@@ -172,12 +172,9 @@ public class ChunkSnapshot
                             statestr.append(pid).append('=').append(prop.get(pid).getAsString());
                         }
                         palette[pi] = DynmapBlockState.getStateByNameAndState(pname, statestr.toString());
-                        //Log.info("getStateByNameAndState(" + pname + "," + statestr.toString() + ")=" + palette[pi]);
-                        //if (pname.contains("grass_block")) { Log.info("getStateByNameAndState(" + pname + "," + statestr.toString() + ")=" + palette[pi]); }
                     }
                     if (palette[pi] == null) {
                         palette[pi] = DynmapBlockState.getBaseStateByName(pname);
-                        //if (pname.contains("grass_block")) { Log.info("getBaseStateByName(" + pname + ")=" + palette[pi]); }
                     }
                     if (palette[pi] == null) {
                         palette[pi] = DynmapBlockState.AIR;
