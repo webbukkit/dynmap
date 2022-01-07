1f75caa7d4c8487bebeb607f22f4da2bad634580
Kosma Moczek <kosma@kosma.pl>
2022-01-07 01:47:06 +0100

    Revert "Merge pull request #3567 from kosma/fabric-1.18-onchunkgenerate"
    This reverts commit ec369e96f821c1e72ac966606f0fbc0c84c26dd2, reversing
    changes made to 9a75210abf6a515eea95f7163230bef981fbf5ad.
    
    Rationale: it will be easier to re-merge the big 1.18 changes after
    Fabric unification has been finished.

--- fabric-1.18/src/main/java/org/dynmap/fabric_1_18/mixin/ThreadedAnvilChunkStorageMixin.java
+++ fabric-1.18/src/main/java/org/dynmap/fabric_1_18/mixin/ThreadedAnvilChunkStorageMixin.java
@@ -1,11 +1,9 @@
 package org.dynmap.fabric_1_18.mixin;
 
-import net.minecraft.server.world.ChunkHolder;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.server.world.ThreadedAnvilChunkStorage;
 import net.minecraft.world.chunk.Chunk;
-import org.dynmap.fabric_1_18.access.ProtoChunkAccessor;
-import org.dynmap.fabric_1_18.event.CustomServerChunkEvents;
+import org.dynmap.fabric_1_18.event.ChunkDataEvents;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -13,20 +11,16 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(value = ThreadedAnvilChunkStorage.class, priority = 666 /* fire before Fabric API CHUNK_LOAD event */)
+@Mixin(ThreadedAnvilChunkStorage.class)
 public abstract class ThreadedAnvilChunkStorageMixin {
-    @Final
     @Shadow
-    ServerWorld world;
+    @Final
+    private ServerWorld world;
 
-    @Inject(
-            /* Same place as fabric-lifecycle-events-v1 event CHUNK_LOAD (we will fire before it) */
-            method = "method_17227",
-            at = @At("TAIL")
-    )
-    private void onChunkGenerate(ChunkHolder chunkHolder, Chunk protoChunk, CallbackInfoReturnable<Chunk> callbackInfoReturnable) {
-        if (((ProtoChunkAccessor)protoChunk).getTouchedByWorldGen()) {
-            CustomServerChunkEvents.CHUNK_GENERATE.invoker().onChunkGenerate(this.world, callbackInfoReturnable.getReturnValue());
+    @Inject(method = "save(Lnet/minecraft/world/chunk/Chunk;)Z", at = @At("RETURN"))
+    private void save(Chunk chunk, CallbackInfoReturnable<Boolean> info) {
+        if (info.getReturnValueZ()) {
+            ChunkDataEvents.SAVE.invoker().onSave(this.world, chunk);
         }
     }
 }
