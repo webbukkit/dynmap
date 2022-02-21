1f8bd818948d65900b276f2bcedbb0a61217f588
Mike Primm <mike@primmhome.com>
2022-02-21 14:15:58 -0600

    Drop 1.11.2 from 3.4 build

--- forge-1.11.2/src/main/java/org/dynmap/forge_1_11_2/ClientProxy.java
+++
@@ -1,6 +0,0 @@
-package org.dynmap.forge_1_11_2;
-
-public class ClientProxy extends Proxy {
-	public ClientProxy() {
-	}
-}
--- forge-1.11.2/src/main/java/org/dynmap/forge_1_11_2/Version.java
+++
@@ -1,7 +0,0 @@
-package org.dynmap.forge_1_11_2;
-
-public class Version {
-    public static final String VER = "@VERSION@";
-    public static final String BUILD_NUMBER = "@BUILD_NUMBER@";
-}
-
