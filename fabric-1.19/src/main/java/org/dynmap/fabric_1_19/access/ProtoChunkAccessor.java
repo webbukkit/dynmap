741633945ebbafb7e5f86027b33175f33727eac3
Clovis de Villars <clovis.devillars@gmail.com>
2022-06-02 14:59:19 +0200

    [WIP] Add support for minecraft 1.19

---
+++ fabric-1.19/src/main/java/org/dynmap/fabric_1_19/access/ProtoChunkAccessor.java
@@ -0,0 +1,5 @@
+package org.dynmap.fabric_1_19.access;
+
+public interface ProtoChunkAccessor {
+    boolean getTouchedByWorldGen();
+}
---
+++ fabric-1.19/src/main/java/org/dynmap/fabric_1_19/command/DynmapExpCommand.java
@@ -0,0 +1,9 @@
+package org.dynmap.fabric_1_19.command;
+
+import org.dynmap.fabric_1_19.DynmapPlugin;
+
+public class DynmapExpCommand extends DynmapCommandExecutor {
+    public DynmapExpCommand(DynmapPlugin p) {
+        super("dynmapexp", p);
+    }
+}
