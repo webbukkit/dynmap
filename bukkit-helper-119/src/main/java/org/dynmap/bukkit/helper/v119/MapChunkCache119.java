9c9c93493ec3fff402ecebe1b069108458be92bc
Mike Primm <mike@primmhome.com>
2022-06-07 12:28:16 -0500

    Initial spigot/paper 1.19 port

---
+++ bukkit-helper-119/src/main/java/org/dynmap/bukkit/helper/v119/MapChunkCache119.java
@@ -0,0 +1,103 @@
+package org.dynmap.bukkit.helper.v119;
+
+import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.craftbukkit.v1_19_R1.CraftServer;
+import org.bukkit.craftbukkit.v1_19_R1.CraftWorld;
+import org.dynmap.DynmapChunk;
+import org.dynmap.bukkit.helper.BukkitVersionHelper;
+import org.dynmap.bukkit.helper.BukkitWorld;
+import org.dynmap.common.chunk.GenericChunk;
+import org.dynmap.common.chunk.GenericChunkCache;
+import org.dynmap.common.chunk.GenericMapChunkCache;
+
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.world.level.ChunkCoordIntPair;
+import net.minecraft.world.level.chunk.storage.ChunkRegionLoader;
+import net.minecraft.world.level.chunk.Chunk;
+
+import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.concurrent.CancellationException;
+import java.util.concurrent.CompletableFuture;
+import java.util.function.Supplier;
+
+/**
+ * Container for managing chunks - dependent upon using chunk snapshots, since rendering is off server thread
+ */
+public class MapChunkCache119 extends GenericMapChunkCache {
+    private static final AsyncChunkProvider119 provider = BukkitVersionHelper.helper.isUnsafeAsync() ? null : new AsyncChunkProvider119();
+    private World w;
+    /**
+     * Construct empty cache
+     */
+    public MapChunkCache119(GenericChunkCache cc) {
+        super(cc);
+    }
+
+    // Load generic chunk from existing and already loaded chunk
+    protected GenericChunk getLoadedChunk(DynmapChunk chunk) {
+        return getLoadedChunk(chunk, false).get();
+    }
+    @Override
+    protected Supplier<GenericChunk> getLoadedChunkAsync(DynmapChunk ch) {
+        return getLoadedChunk(ch, true);
+    }
+
+    @Override
+    protected Supplier<GenericChunk> loadChunkAsync(DynmapChunk chunk){
+        try {
+            CompletableFuture<NBTTagCompound> nbt = provider.getChunk(((CraftWorld) w).getHandle(), chunk.x, chunk.z);
+            return () -> {
+                NBTTagCompound compound = nbt.join();
+                return compound == null ? null : parseChunkFromNBT(new NBT.NBTCompound(compound));
+            };
+        } catch (InvocationTargetException | IllegalAccessException ignored) {
+            return () -> null;
+        }
+    }
+
+    private Supplier<GenericChunk> getLoadedChunk(DynmapChunk chunk, boolean async) {
+        CraftWorld cw = (CraftWorld) w;
+        if (!cw.isChunkLoaded(chunk.x, chunk.z)) return () -> null;
+        Chunk c = cw.getHandle().getChunkIfLoaded(chunk.x, chunk.z); //already safe async on vanilla
+        if ((c == null) || c.o) return () -> null;    // c.loaded
+        if (async) { //the data of the chunk may change while we write, better to write it sync
+            CompletableFuture<NBTTagCompound> nbt = CompletableFuture.supplyAsync(() -> ChunkRegionLoader.a(cw.getHandle(), c), ((CraftServer) Bukkit.getServer()).getServer());
+            return () -> {
+                    NBTTagCompound compound = nbt.join();
+                    return compound == null ? null : parseChunkFromNBT(new NBT.NBTCompound(compound));
+            };
+        } else {
+            NBTTagCompound nbt = ChunkRegionLoader.a(cw.getHandle(), c);
+            GenericChunk genericChunk;
+            if (nbt != null) genericChunk = parseChunkFromNBT(new NBT.NBTCompound(nbt));
+            else genericChunk = null;
+            return () -> genericChunk;
+        }
+
+    }
+    // Load generic chunk from unloaded chunk
+    protected GenericChunk loadChunk(DynmapChunk chunk) {
+        CraftWorld cw = (CraftWorld) w;
+        NBTTagCompound nbt = null;
+        ChunkCoordIntPair cc = new ChunkCoordIntPair(chunk.x, chunk.z);
+        GenericChunk gc = null;
+        try {	// BUGBUG - convert this all to asyn properly, since now native async
+            nbt = cw.getHandle().k().a.f(cc).join().get();	// playerChunkMap
+        } catch (CancellationException cx) {
+        } catch (NoSuchElementException snex) {
+        }
+        if (nbt != null) {
+            gc = parseChunkFromNBT(new NBT.NBTCompound(nbt));
+        }
+        return gc;
+    }
+
+    public void setChunks(BukkitWorld dw, List<DynmapChunk> chunks) {
+        this.w = dw.getWorld();
+        super.setChunks(dw, chunks);
+    }
+}
---
+++ bukkit-helper-119/src/main/java/org/dynmap/bukkit/helper/v119/NBT.java
@@ -0,0 +1,126 @@
+package org.dynmap.bukkit.helper.v119;
+
+import org.dynmap.common.chunk.GenericBitStorage;
+import org.dynmap.common.chunk.GenericNBTCompound;
+import org.dynmap.common.chunk.GenericNBTList;
+
+import java.util.Set;
+import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.nbt.NBTTagList;
+import net.minecraft.util.SimpleBitStorage;
+
+public class NBT {
+
+	public static class NBTCompound implements GenericNBTCompound {
+		private final NBTTagCompound obj;
+		public NBTCompound(NBTTagCompound t) {
+			this.obj = t;
+		}
+		@Override
+		public Set<String> getAllKeys() {
+			return obj.d();
+		}
+		@Override
+		public boolean contains(String s) {
+			return obj.e(s);
+		}
+		@Override
+		public boolean contains(String s, int i) {
+			return obj.b(s, i);
+		}
+		@Override
+		public byte getByte(String s) {
+			return obj.f(s);
+		}
+		@Override
+		public short getShort(String s) {
+			return obj.g(s);
+		}
+		@Override
+		public int getInt(String s) {
+			return obj.h(s);
+		}
+		@Override
+		public long getLong(String s) {
+			return obj.i(s);
+		}
+		@Override
+		public float getFloat(String s) {
+			return obj.j(s);
+		}
+		@Override
+		public double getDouble(String s) {
+			return obj.k(s);
+		}
+		@Override
+		public String getString(String s) {
+			return obj.l(s);
+		}
+		@Override
+		public byte[] getByteArray(String s) {
+			return obj.m(s);
+		}
+		@Override
+		public int[] getIntArray(String s) {
+			return obj.n(s);
+		}
+		@Override
+		public long[] getLongArray(String s) {
+			return obj.o(s);
+		}
+		@Override
+		public GenericNBTCompound getCompound(String s) {
+			return new NBTCompound(obj.p(s));
+		}
+		@Override
+		public GenericNBTList getList(String s, int i) {
+			return new NBTList(obj.c(s, i));
+		}
+		@Override
+		public boolean getBoolean(String s) {
+			return obj.q(s);
+		}
+		@Override
+		public String getAsString(String s) {
+			return obj.c(s).e_();
+		}
+		@Override
+		public GenericBitStorage makeBitStorage(int bits, int count, long[] data) {
+			return new OurBitStorage(bits, count, data);
+		}
+		public String toString() {
+			return obj.toString();
+		}
+	}
+	public static class NBTList implements GenericNBTList {
+		private final NBTTagList obj;
+		public NBTList(NBTTagList t) {
+			obj = t;
+		}
+		@Override
+		public int size() {
+			return obj.size();
+		}
+		@Override
+		public String getString(int idx) {
+			return obj.j(idx);
+		}
+		@Override
+		public GenericNBTCompound getCompound(int idx) {
+			return new NBTCompound(obj.a(idx));
+		}
+		public String toString() {
+			return obj.toString();
+		}
+	}
+	public static class OurBitStorage implements GenericBitStorage {
+		private final SimpleBitStorage bs;
+		public OurBitStorage(int bits, int count, long[] data) {
+			bs = new SimpleBitStorage(bits, count, data);
+		}
+		@Override
+		public int get(int idx) {
+			return bs.a(idx);
+		}
+	}
+}
