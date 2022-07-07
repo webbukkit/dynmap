17e60cff40b5bfbdb9a65816ac1b5c1031ea09d3
mastermc05 <masmc05@gmail.com>
2022-07-07 15:34:16 +0300

    fix 1.19

--- bukkit-helper-119/src/main/java/org/dynmap/bukkit/helper/v119/AsyncChunkProvider119.java
+++ bukkit-helper-119/src/main/java/org/dynmap/bukkit/helper/v119/AsyncChunkProvider119.java
@@ -1,31 +1,53 @@
 package org.dynmap.bukkit.helper.v119;
 
 import net.minecraft.nbt.NBTTagCompound;
+import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.WorldServer;
+import net.minecraft.world.level.chunk.Chunk;
+import net.minecraft.world.level.chunk.IChunkAccess;
+import net.minecraft.world.level.chunk.storage.ChunkRegionLoader;
+import org.bukkit.Bukkit;
+import org.bukkit.craftbukkit.v1_19_R1.CraftServer;
+import org.bukkit.craftbukkit.v1_19_R1.CraftWorld;
+import org.dynmap.MapManager;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 /**
  * The provider used to work with paper libs
  * Because paper libs need java 17 we can't interact with them directly
  */
+@SuppressWarnings({"JavaReflectionMemberAccess"}) //java don't know about paper
 public class AsyncChunkProvider119 {
     private final Thread ioThread;
     private final Method getChunk;
     private final Predicate<NBTTagCompound> ifFailed;
-    AsyncChunkProvider119 () {
+    private final Method getAsyncSaveData;
+    private final Method save;
+    private int currTick = MinecraftServer.currentTick;
+    private int currChunks = 0;
+
+    AsyncChunkProvider119() {
         try {
             Predicate<NBTTagCompound> ifFailed1 = null;
-            Method getChunk1 = null;
+            Method getChunk1 = null, getAsyncSaveData1 = null, save1 = null;
             Thread ioThread1 = null;
             try {
                 Class<?> threadClass = Class.forName("com.destroystokyo.paper.io.PaperFileIOThread");
+                Class<?> asyncChunkData = Arrays.stream(ChunkRegionLoader.class.getClasses())
+                        .filter(c -> c.getSimpleName().equals("AsyncSaveData"))
+                        .findFirst()
+                        .orElseThrow(RuntimeException::new);
+                getAsyncSaveData1 = ChunkRegionLoader.class.getMethod("getAsyncSaveData", WorldServer.class, IChunkAccess.class);
+                save1 = ChunkRegionLoader.class.getMethod("saveChunk", WorldServer.class, IChunkAccess.class, asyncChunkData);
                 Class<?>[] classes = threadClass.getClasses();
                 Class<?> holder = Arrays.stream(classes).filter(aClass -> aClass.getSimpleName().equals("Holder")).findAny().orElseThrow(RuntimeException::new);
                 ioThread1 = (Thread) holder.getField("INSTANCE").get(null);
@@ -35,6 +57,8 @@ public class AsyncChunkProvider119 {
             } catch (ClassNotFoundException | NoSuchFieldException | IllegalAccessException | NoSuchMethodException e) {
                 e.printStackTrace();
             }
+            getAsyncSaveData = Objects.requireNonNull(getAsyncSaveData1);
+            save = Objects.requireNonNull(save1);
             ifFailed = Objects.requireNonNull(ifFailed1);
             getChunk = Objects.requireNonNull(getChunk1);
             ioThread = Objects.requireNonNull(ioThread1);
@@ -56,5 +80,39 @@ public class AsyncChunkProvider119 {
             }
             return null;
         });
+    }
+
+    public synchronized Supplier<NBTTagCompound> getLoadedChunk(CraftWorld world, int x, int z) {
+        if (!world.isChunkLoaded(x, z)) return () -> null;
+        Chunk c = world.getHandle().getChunkIfLoaded(x, z); //already safe async on vanilla
+        if ((c == null) || c.o) return () -> null;    // c.loaded
+        if (currTick != MinecraftServer.currentTick) {
+            currTick = MinecraftServer.currentTick;
+            currChunks = 0;
+        }
+        //prepare data synchronously
+        CompletableFuture<?> future = CompletableFuture.supplyAsync(() -> {
+            try {
+                return getAsyncSaveData.invoke(null, world.getHandle(), c);
+            } catch (ReflectiveOperationException e) {
+                throw new RuntimeException(e);
+            }
+        }, ((CraftServer) Bukkit.getServer()).getServer());
+        //we shouldn't stress main thread
+        if (++currChunks > MapManager.mapman.getMaxChunkLoadsPerTick()) {
+            try {
+                Thread.sleep(25); //hold the lock so other threads also won't stress main thread
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        }
+        //save data asynchronously
+        return () -> {
+            try {
+                return (NBTTagCompound) save.invoke(null, world.getHandle(), c, future.get());
+            } catch (ReflectiveOperationException | ExecutionException | InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+        };
     }
 }
