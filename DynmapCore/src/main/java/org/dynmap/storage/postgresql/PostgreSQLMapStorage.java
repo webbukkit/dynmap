b331cfaf272f66bd00bf80126987d10c1ea78858
Mike Primm <mike@primmhome.com>
2022-07-30 21:13:59 -0500

    Fix exception in processEnumMapTiles for Postgres

--- DynmapCore/src/main/java/org/dynmap/storage/postgresql/PostgreSQLMapStorage.java
+++ DynmapCore/src/main/java/org/dynmap/storage/postgresql/PostgreSQLMapStorage.java
@@ -708,7 +708,7 @@ public class PostgreSQLMapStorage extends MapStorage {
             while (!done) {
 	            // Query tiles for given mapkey
 	            Statement stmt = c.createStatement();
-	            ResultSet rs = stmt.executeQuery(String.format("SELECT x,y,zoom,Format FROM %s WHERE MapID=%d OFFSET %d LIMIT %d;", tableTiles, mapKey, offset, limit));
+	            ResultSet rs = stmt.executeQuery(String.format("SELECT x,y,zoom,Format FROM %s WHERE MapID=%d OFFSET %d LIMIT %d;", tableTiles, mapkey, offset, limit));
 	            int cnt = 0;
 	            while (rs.next()) {
 	                StorageTile st = new StorageTile(world, map, rs.getInt("x"), rs.getInt("y"), rs.getInt("zoom"), var);
