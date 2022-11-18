a6ead1bc3d485485684ce7b1811498099f14ec9c
mastermc05 <masmc05@gmail.com>
2022-11-18 13:27:07 +0200

    Fabric 1.16.4 (1.16.5)

--- fabric-1.16.4/src/main/java/org/dynmap/fabric_1_16_4/mixin/BiomeEffectsAccessor.java
+++ fabric-1.16.4/src/main/java/org/dynmap/fabric_1_16_4/mixin/BiomeEffectsAccessor.java
@@ -4,8 +4,16 @@ import net.minecraft.world.biome.BiomeEffects;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
 
+import java.util.Optional;
+
 @Mixin(BiomeEffects.class)
 public interface BiomeEffectsAccessor {
     @Accessor
     int getWaterColor();
+    @Accessor
+    Optional<Integer> getFoliageColor();
+    @Accessor
+    Optional<Integer> getGrassColor();
+    @Accessor
+    BiomeEffects.GrassColorModifier getGrassColorModifier();
 }
