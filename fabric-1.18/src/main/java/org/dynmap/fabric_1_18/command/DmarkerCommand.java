a6548ec0b7155c11be7f24c72363bcda8ecf1e41
Michael Primm <mike@primmhome.com>
2022-12-12 00:45:14 -0600

    Drop Fabric and Forge 1.18 builds (just stick with 1.18.2)

--- fabric-1.18/src/main/java/org/dynmap/fabric_1_18/command/DmarkerCommand.java
+++
@@ -1,9 +0,0 @@
-package org.dynmap.fabric_1_18.command;
-
-import org.dynmap.fabric_1_18.DynmapPlugin;
-
-public class DmarkerCommand extends DynmapCommandExecutor {
-    public DmarkerCommand(DynmapPlugin p) {
-        super("dmarker", p);
-    }
-}
--- forge-1.18/src/main/java/org/dynmap/forge_1_18/ClientProxy.java
+++
@@ -1,6 +0,0 @@
-package org.dynmap.forge_1_18;
-
-public class ClientProxy extends Proxy {
-	public ClientProxy() {
-	}
-}
