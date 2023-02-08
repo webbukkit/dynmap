cc88883dc18f79fb0f4c9196289127008d7f2a0a
Jurgen <intern@ampc-solutions.com>
2023-02-08 19:58:47 +0100

    fixed imports, thanks intellij

--- DynmapCore/src/main/java/org/dynmap/hdmap/CaveHDShader.java
+++ DynmapCore/src/main/java/org/dynmap/hdmap/CaveHDShader.java
@@ -6,7 +6,10 @@ import java.io.IOException;
 import java.util.BitSet;
 import java.util.List;
 
-import org.dynmap.*;
+import org.dynmap.Color;
+import org.dynmap.ConfigurationNode;
+import org.dynmap.DynmapCore;
+import org.dynmap.MapManager;
 import org.dynmap.common.DynmapCommandSender;
 import org.dynmap.exporter.OBJExport;
 import org.dynmap.renderer.DynmapBlockState;
