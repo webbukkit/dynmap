098081f2f3023adb679405f470c0f3e9b139ddcd
Kosma Moczek <kosma@kosma.pl>
2023-03-14 21:00:03 +0100

    fabric-1.19.4: copy from fabric-1.19.3

---
+++ fabric-1.19.4/src/main/java/org/dynmap/fabric_1_19_3/command/DmarkerCommand.java
@@ -0,0 +1,9 @@
+package org.dynmap.fabric_1_19_3.command;
+
+import org.dynmap.fabric_1_19_3.DynmapPlugin;
+
+public class DmarkerCommand extends DynmapCommandExecutor {
+    public DmarkerCommand(DynmapPlugin p) {
+        super("dmarker", p);
+    }
+}
