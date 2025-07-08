5385ac29a490b0073b34893c4a4cc6f67ba14347
ReuS <kahlumba@protonmail.com>
2025-07-09 00:40:23 +1100

    Fabric 1.21.7 update

---
+++ fabric-1.21.7/src/main/java/org/dynmap/fabric_1_21_7/command/DmapCommand.java
@@ -0,0 +1,9 @@
+package org.dynmap.fabric_1_21_7.command;
+
+import org.dynmap.fabric_1_21_7.DynmapPlugin;
+
+public class DmapCommand extends DynmapCommandExecutor {
+    public DmapCommand(DynmapPlugin p) {
+        super("dmap", p);
+    }
+}
---
+++ fabric-1.21.7/src/main/java/org/dynmap/fabric_1_21_7/command/DynmapCommand.java
@@ -0,0 +1,9 @@
+package org.dynmap.fabric_1_21_7.command;
+
+import org.dynmap.fabric_1_21_7.DynmapPlugin;
+
+public class DynmapCommand extends DynmapCommandExecutor {
+    public DynmapCommand(DynmapPlugin p) {
+        super("dynmap", p);
+    }
+}
